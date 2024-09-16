6f0c3d0e9bdfeecdcb5676ae8344e361a2739837
MasuRii マス リ <kanjiharigana@gmail.com>
2024-09-16 12:03:47 +0800

    first commit

---
+++ src/main/java/com/g1appdev/eduprojectmanagementsystem/EduprojectmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.g1appdev.eduprojectmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduprojectmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduprojectmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g1appdev/eduprojectmanagementsystem/EduprojectmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1appdev.eduprojectmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduprojectmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
